c90333d5f1c3704429ea8848395de28634d0cd3f
simsononroad <d-github@kopaszhegy.hu>
2024-10-28 20:24:12 +0100

    vegleges

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 from os import system
 from time import sleep as wait
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+#from pathlib import path
 
 is_app_running = True
-
+debugging = True
 page = "index.hu"
 tag = "h2"
 Class = "cikkcim"
+tag1 = "div"
+Class1 = "img-container"
+
+chrome_option = Options()
+if debugging:
+    chrome_option.add_experimental_option("detach", True)
+else:
+    chrome_option.add_argument("--headless")
 
 
 
@@ -16,8 +30,27 @@ def scrape():
     page_to_scrape = requests.get(f"https://{page}")
     wait(0.1)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    quotes = soup.find_all(tag, attrs={"class":Class})
-    print(quotes[0].text)
+    focim = soup.find_all(tag, attrs={"class":Class})
+    focim1 = soup.find_all(tag1, attrs={"class":Class1})
+    
+    
+    print("--------Legújabb hírek-------")
+    print(f"{focim[0].text}")
+    what_open = input("Cikk megnyitása (i/n)\n>>>")
+    if what_open == "i":
+        driver = webdriver.Chrome(options=chrome_option)
+        driver.get("https://index.hu")
+        links = driver.find_element("xpath", "//div[contains(@class, 'cim')]")
+        links.click()
+    else:
+        system("clear")
+        print(f"{focim[0].text}")
+        
+        
+        
+        
+    if not debugging:
+        driver.quit()       
 
     
 
